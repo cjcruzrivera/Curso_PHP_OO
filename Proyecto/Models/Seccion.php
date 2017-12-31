b4f2b4defff8f595f05dedb2b091b8c1d9fd46d1
Camilo José Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2017-12-31 04:03:45 -0500

    Creacion del modelo

---
+++ Proyecto/Models/Seccion.php
@@ -0,0 +1,51 @@
+<?php namespace Models;
+
+    class Seccion{
+
+        //Atributos
+        private $id;
+        private $nombre;
+        private $con;
+
+        //Metodos
+        public function __construct(){
+            $this->con = new Conexion();
+        }
+        
+        public function set($atr, $valor){
+            $this->$atr = $valor;
+        }
+
+        public function get($atr){
+            return $this->$atr;
+        }
+
+        public function listar(){
+            $sql = "SELECT * FROM seccion";
+            $result = $this->con->query_result($sql);
+            return $result;
+        }
+
+        public function view(){
+            $sql = "SELECT * FROM seccion WHERE id = $this->id";
+            $result = $this->con->query_result($sql);
+            return pg_fetch_assoc($result);            
+        }
+
+        public function add(){
+            $sql = "INSERT INTO seccion(nombre) VALUES ('$this->nombre')";            
+            $this->con->query_simple($sql);
+        }
+
+        public function delete(){
+            $sql = "DELETE FROM seccion WHERE id = $this->id";
+            $this->con->query_simple($sql);            
+        }
+
+        public function edit(){
+            $sql = "UPDATE FROM seccion SET nombre = '$this->nombre' WHERE id = $this->id";
+            $this->con->query_simple($sql);            
+        }
+
+    }
+?>
